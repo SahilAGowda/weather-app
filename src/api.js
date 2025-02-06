9e3389aaa14b43a357687abe0760fddce38ecf96
SahilAGowda <sahilagowda.22cs134.nc@cambridge.edu.in>
2025-02-07 04:44:53 +0530

    weather-app completed

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "66950f80demsh1970cc564bfae0cp19b798jsn40a16225db44",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+  export const WEATHER_API_KEY = "02cf6ff0ed9c9dbbcaeed254c8c436f4"
+  
